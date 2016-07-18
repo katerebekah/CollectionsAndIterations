472391e5b32a9e8811e1650d94f0fd9be2a519ab
Kate <katerebekah@gmail.com>
2016-07-18 15:31:25 -0500

    initial file setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionsAndIteration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
